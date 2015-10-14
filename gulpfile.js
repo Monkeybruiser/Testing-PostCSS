1b907bf8b5c1960c93c77431f10397eb787e620c
Richard Walker <rich@cargocreative.co.uk>
2015-10-14 15:39:14 +0100

    Initial extra plugins install

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,21 @@ var gulp				= require('gulp');
 var postcss				= require('gulp-postcss');
 
 
+// Autoprefixer, CSSNEXT and PreCSS
+var autoprefixer 		= require('autoprefixer');
+var cssnext				= require('cssnext');
+var precss				= require('precss');
+
+
 
 
 gulp.task('css', function () {
-  var processors = [
-  ];
-  return gulp.src('./src/*.css')
-    .pipe(postcss(processors))
-    .pipe(gulp.dest('./public/library/css'));
+	var processors = [
+		autoprefixer,
+		cssnext,
+		precss
+	];
+	return gulp.src('./src/*.css')
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('./public/library/css'));
 });
